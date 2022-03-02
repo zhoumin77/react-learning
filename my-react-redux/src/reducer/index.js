25b4a513f6b6caaab8e9f2321e8ff1ac9e06c04c
zhoumin <zhoumin@shopex.cn>
2022-03-02 10:47:40 +0800

    feat: react redux 优化

--- my-react-redux/src/reducer/index.js
+++ my-react-redux/src/reducer/index.js
@@ -1,28 +1,39 @@
 import { createStore, combineReducers } from 'redux';
-function time(time = [], action) {
-  // console.log('time',time);
-  // console.log('action',action);
+function time(time = {
+  id: []
+}, action) {
+//   state 默认值要给一个对象
+  let id = time.id;
   switch (action.type) {
     case 'TIME__ADD':
-      time.push({
-        id: Date.now()
-      })
-      console.log(...time, 1);
-      return [...time] // 这里一定要返回一个新的对象，否则会有不更新的问题，因为是浅比较
+      // console.log({...time}, 1);
+      id.push(Date.now())
+      return {...time} // 这里一定要返回一个新的对象，否则会有不更新的问题，因为是浅比较
     default:
-      return [...time]
+      return {...time} 
   }
 }
 
-function num(num = 0, action) {
+function num(num = {
+  number: 0
+}, action) {
+  // let number = num.number; // 如果这样写的话，是新增了一个 number的变量和原来的 num 没有关系！
   switch (action.type) {
     case 'NUM_ADD': // 需要保证和其他reducer的action.type 不重名！！！
-      return num++;
+      num.number++;
+      return {...num}
     default:
-      return num
+      return {...num}
   }
 }
 
+// combineReducers 相当于
+// function reducer(state={},action){
+//   return {
+//     time: time(state.list,action),
+//     num: num(state.num,action)
+//   }
+// }
 const reducer = combineReducers({
   time,
   num
--- my-react-redux/src/reduxHook.js
+++ my-react-redux/src/reduxHook.js
@@ -5,18 +5,29 @@ function ReduxHook() {
   const state = useSelector(
     state => {
       console.log('state1', state);
-      return state.time
+      return state // 注意 这里是进行渐层比较的，如果写成 time.id 的话，就不会识别到更新，因为reducer里面返回的是一个新的time
     }
   )
+  const timeId = state.time.id
+  const num = state.num.number
   console.log('state2', state);
   const dispatch = useDispatch();
   return (
     <div>
-      {state.map((item) => item.id + '<br/>')}
+      time:
+      {timeId.map((item) => item + '<br/>')}
+      <hr />
+
+
+      num: {num}
       <hr />
 
       <button onClick={() => { dispatch({ type: 'TIME__ADD' }) }}>
         hook按钮
+      </button>
+
+      <button onClick={() => { dispatch({ type: 'NUM_ADD' }) }}>
+        hook num 按钮
       </button>
     </div>
   )
