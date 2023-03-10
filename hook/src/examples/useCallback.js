9b61467a533f2493f0ba6a52dbc527139fa1710d
zhoumin <zhoumin@shopex.cn>
2023-03-10 11:42:55 +0800

    no message

--- hook/src/examples/useCallback.js
+++ hook/src/examples/useCallback.js
@@ -37,10 +37,16 @@ function Index() {
    * 注意： 第一个函数里面，要是一个执行函数或者要做的事情
    */
 
-  // 只有val改变的时候，才会重新计算handleClick，不会生成多余的函数对象
+  // 只有val改变的时候，才会重新计算handleClick，不会生成多余的函数对象，handleClick不重新渲染，子组件就不会重新渲染
   const handleClick = useCallback(() => {
-    console.log("click");
+    console.log("click");  // 页面渲染的时候并没有执行，点击的时候才触发
   }, [val]);
+
+
+  // 如果写成这样，当val和num改变，也就是父组件改变的时候，子组件就会重新渲染
+  // const handleClick = () => {
+  //   console.log("click");  // 页面渲染的时候并没有执行，点击的时候才触发
+  // };
 
   return (
     <div>
