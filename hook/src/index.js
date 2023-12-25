0a534d13aa68cf62eb200b6e3612c680de62d9f6
zhoumin <zhoumin@shopex.cn>
2023-12-25 16:18:54 +0800

    nomessage

--- hook/src/index.js
+++ hook/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './examples/memo';
-// import App from './examples/useMemo';
+import App from './examples/useMemo';
 // import App from './examples/useCallback';
-import App from './examples/useEffect';
+// import App from './examples/useEffect';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
